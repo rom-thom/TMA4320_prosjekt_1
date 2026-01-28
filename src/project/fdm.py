2b2e9a2070ed6d78d2f009d8f500ad9ddc57ef7f
Thomas <thomapau@stud.ntnu.no>
2026-01-28 14:09:48 +0100

    Continued work

--- src/project/fdm.py
+++ src/project/fdm.py
@@ -36,10 +36,11 @@ def solve_heat_equation(
     # Placeholder initialization — replace this with your implementation
     T = np.zeros((cfg.nt, cfg.nx, cfg.ny))
 
-    A = _build_matrix(cfg, dx, dy, dt)
-    b = _build_rhs(cfg, t, X, Y, dx, dy, dt, ) # Test
-    # Løyser A * T_temp = b, for T_temp
-    T_temp = np.linalg.solve(A, b)
+    for index_t, t_i in enumerate(t[:-1]):
+        A = _build_matrix(cfg, dx, dy, dt)
+        b = _build_rhs(cfg, t_i, X, Y, dx, dy, dt, t[index_t +1])
+        # Løyser A * T_temp = b, for T_temp
+        T_temp = np.linalg.solve(A, b).reshape()
 
     #######################################################################
     # Oppgave 3.2: Slutt
